c551896c34adab5ab93a6a09d40b589b0427eac7
willbe <rlakim5521@naver.com>
2019-01-04 21:29:56 +0900

    data_generator 개선
    1. USED_PAST_MONTHS를 바꿀 수 있도록 함
    2. training_set과 test_set이 언제나 같도록 함
    3. prediction이 model에 따라 해당하는 model의 폴더에 저장되도록 함

--- data_generator.py
+++ data_generator.py
@@ -9,6 +9,7 @@ from ksif.core.columns import *
 from sklearn.preprocessing import MinMaxScaler
 
 START_DATE = '2007-04-30'
+USED_PAST_MONTHS = 12  # At a time, use past 12 months data and current month data.
 
 scaler = MinMaxScaler()
 
@@ -18,34 +19,11 @@ def get_data_set(portfolio, rolling_columns):
     rolled_columns = []
     data_set = portfolio.reset_index(drop=True)
     for column in rolling_columns:
-        t_0 = column + '_t'
-        t_1 = column + '_t-1'
-        t_2 = column + '_t-2'
-        t_3 = column + '_t-3'
-        t_4 = column + '_t-4'
-        t_5 = column + '_t-5'
-        t_6 = column + '_t-6'
-        t_7 = column + '_t-7'
-        t_8 = column + '_t-8'
-        t_9 = column + '_t-9'
-        t_10 = column + '_t-10'
-        t_11 = column + '_t-11'
-        t_12 = column + '_t-12'
-        result_columns.extend([t_0, t_1, t_2, t_3, t_4, t_5, t_6, t_7, t_8, t_9, t_10, t_11, t_12])
-        rolled_columns.extend([t_0, t_1, t_2, t_3, t_4, t_5, t_6, t_7, t_8, t_9, t_10, t_11, t_12])
-        data_set[t_0] = data_set[column]
-        data_set[t_1] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(1)).reset_index(drop=True)
-        data_set[t_2] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(2)).reset_index(drop=True)
-        data_set[t_3] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(3)).reset_index(drop=True)
-        data_set[t_4] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(4)).reset_index(drop=True)
-        data_set[t_5] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(5)).reset_index(drop=True)
-        data_set[t_6] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(6)).reset_index(drop=True)
-        data_set[t_7] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(7)).reset_index(drop=True)
-        data_set[t_8] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(8)).reset_index(drop=True)
-        data_set[t_9] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(9)).reset_index(drop=True)
-        data_set[t_10] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(10)).reset_index(drop=True)
-        data_set[t_11] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(11)).reset_index(drop=True)
-        data_set[t_12] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(12)).reset_index(drop=True)
+        for i in range(0, USED_PAST_MONTHS + 1):
+            column_i = column + '_t-{}'.format(i)
+            result_columns.append(column_i)
+            rolled_columns.append(column_i)
+            data_set[column_i] = data_set.groupby(by=[CODE]).apply(lambda x: x[column].shift(i)).reset_index(drop=True)
     data_set = data_set[result_columns]
     data_set = data_set.dropna().reset_index(drop=True)
     data_set[rolled_columns] = scaler.fit_transform(data_set[rolled_columns])
@@ -54,10 +32,10 @@ def get_data_set(portfolio, rolling_columns):
 
 
 def save_all():
-    columns = [DATE, CODE, RET_1]
+    fixed_columns = [DATE, CODE, RET_1]
     rolling_columns = [E_P, B_P, S_P, C_P, OP_P, GP_P, ROA, ROE, QROA, QROE, GP_A, ROIC, GP_S, SALESQOQ, GPQOQ, ROAQOQ,
                        MOM6, MOM12, BETA_1D, VOL_5M, LIQ_RATIO, EQUITY_RATIO, DEBT_RATIO, FOREIGN_OWNERSHIP_RATIO]
-    columns.extend(rolling_columns)
+    fixed_columns.extend(rolling_columns)
     all_portfolio = Portfolio(start_date=START_DATE)
     # 최소 시가총액 100억
     all_portfolio = all_portfolio.loc[all_portfolio[MKTCAP] > 10000000000, :]
