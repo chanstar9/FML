27c1b5fd4e3f4409e6646fd15d53beb329d029a2
Jaekyoung Kim <jaekyoungkim@kaist.ac.kr>
2019-01-18 14:30:44 +0900

    Release GPU Memory after work

--- runner.py
+++ runner.py
@@ -6,44 +6,19 @@
 from model import *
 from settings import *
 
-# params = []
-#
-# for data_set in sorted(data_sets.keys()):
-#     for activation in sorted(activations.keys()):
-#         for bias_initializer in sorted(initializers.keys()):
-#             for kernel_initializer in sorted(initializers.keys()):
-#                 for bias_regularizer in sorted(regularizers.keys()):
-#                     for hidden_layer in sorted(hidden_layers.keys()):
-#                         params.append({
-#                             TRAINING_SET: data_set,
-#                             TEST_SET: data_set,
-#                             BATCH_SIZE: 300,
-#                             EPOCHS: 100,
-#                             ACTIVATION: activation,
-#                             BIAS_INITIALIZER: bias_initializer,
-#                             KERNEL_INITIALIZER: kernel_initializer,
-#                             BIAS_REGULARIZER: bias_regularizer,
-#                             HIDDEN_LAYER: hidden_layer,
-#                             DROPOUT: False,
-#                             DROPOUT_RATE: 0.5
-#                         })
 
 if __name__ == '__main__':
 
-    # case_number = 0
-    # print('Case: {}'.format(case_number))
-    # print('Param: {}'.format(params[case_number]))
-    # simulate(param=params[case_number], case_number=case_number)
-
-    simulate(param={
-        DATA_SET: ALL,
-        BATCH_SIZE: 10000,
-        EPOCHS: 100,
-        ACTIVATION: LINEAR,
-        BIAS_INITIALIZER: HE_UNIFORM,
-        KERNEL_INITIALIZER: GLOROT_UNIFORM,
-        BIAS_REGULARIZER: NONE,
-        HIDDEN_LAYER: DNN8_3,
-        DROPOUT: True,
-        DROPOUT_RATE: 0.5
-    }, case_number=9)
+    for i in range(6, 10):
+        simulate(param={
+            DATA_SET: ALL,
+            BATCH_SIZE: 10000,
+            EPOCHS: 100,
+            ACTIVATION: RELU,
+            BIAS_INITIALIZER: HE_UNIFORM,
+            KERNEL_INITIALIZER: GLOROT_UNIFORM,
+            BIAS_REGULARIZER: NONE,
+            HIDDEN_LAYER: DNN8_4,
+            DROPOUT: True,
+            DROPOUT_RATE: 0.5
+        }, case_number=i)
