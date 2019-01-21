a1c19658f3b0017dbcaf9230ea677d7dd531e107
Jaekyoung Kim <jaekyoungkim@kaist.ac.kr>
2019-01-21 22:19:37 +0900

    Adjsut a lazy loading dictionary to settings

--- runner.py
+++ runner.py
@@ -9,16 +9,15 @@ from settings import *
 
 if __name__ == '__main__':
 
-    for i in range(8, 10):
-        simulate(param={
-            DATA_SET: ALL,
-            BATCH_SIZE: 10000,
-            EPOCHS: 100,
-            ACTIVATION: RELU,
-            BIAS_INITIALIZER: HE_UNIFORM,
-            KERNEL_INITIALIZER: GLOROT_UNIFORM,
-            BIAS_REGULARIZER: NONE,
-            HIDDEN_LAYER: DNN8_2,
-            DROPOUT: False,
-            DROPOUT_RATE: 0.5
-        }, case_number=i)
+    simulate(param={
+        DATA_SET: ALL,
+        BATCH_SIZE: 10000,
+        EPOCHS: 100,
+        ACTIVATION: RELU,
+        BIAS_INITIALIZER: HE_UNIFORM,
+        KERNEL_INITIALIZER: GLOROT_UNIFORM,
+        BIAS_REGULARIZER: NONE,
+        HIDDEN_LAYER: DNN8_4,
+        DROPOUT: False,
+        DROPOUT_RATE: 0.5
+    }, case_number=9)
