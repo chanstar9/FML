d09cafe21154f8a84cb4111a0b0048ca77a95eb3
willbe <rlakim5521@naver.com>
2019-01-21 20:07:25 +0900

    BUG FIX

--- universe_number.py
+++ universe_number.py
@@ -8,14 +8,14 @@ from data_generator import *
 all_count = pd.read_csv('data/all.csv').groupby(by=[DATE])[CODE].count()
 filter_count = pd.read_csv('data/filter.csv').groupby(by=[DATE])[CODE].count()
 bollinger_count = pd.read_csv('data/bollinger.csv').groupby(by=[DATE])[CODE].count()
-# sector_count = pd.read_csv('data/sector.csv').groupby(by=[DATE])[CODE].count()
+sector_count = pd.read_csv('data/sector.csv').groupby(by=[DATE])[CODE].count()
 
 
 result = pd.DataFrame(data={
     ALL: all_count,
     FILTER: filter_count,
     BOLLINGER: bollinger_count,
-    # SECTOR: sector_count,
+    SECTOR: sector_count,
 }).fillna(0)
 
 if __name__ == '__main__':
