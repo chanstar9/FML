685f769d3108d660ff997918d3996cf45c0b65e3
willbe <rlakim5521@naver.com>
2019-02-28 00:56:42 +0900

    Add old_data flag for efficient data generating

--- data_generator.py
+++ data_generator.py
@@ -58,33 +58,39 @@ def get_data_set(portfolio, rolling_columns, dummy_columns=None, return_y=True, 
     return data_set
 
 
-def save_data(portfolio: Portfolio, data_name: str, rolling_columns: list, dummy_columns: list = None,
+def save_data(old_data: bool, portfolio: Portfolio, data_name: str, rolling_columns: list, dummy_columns: list = None,
               filtering_dataframe=None):
     print("Start saving {}...".format(data_name))
 
     portfolio = portfolio.sort_values(by=[CODE, DATE]).reset_index(drop=True)
 
-    # old data
-    # RET_1이 존재하지 않는 마지막 달 제거
-    old_portfolio = portfolio.loc[~pd.isna(portfolio[RET_1]), :]
-    old_set = get_data_set(old_portfolio, rolling_columns, dummy_columns)
+    if old_data:
+        # old data
+        # RET_1이 존재하지 않는 마지막 달 제거
+        old_portfolio = portfolio.loc[~pd.isna(portfolio[RET_1]), :]
+        old_set = get_data_set(old_portfolio, rolling_columns, dummy_columns)
 
-    # recent data
-    recent_set = get_data_set(portfolio, rolling_columns, dummy_columns, return_y=False)
-    # 마지막 달만 사용
-    last_month = np.sort(recent_set[DATE].unique())[-1]
-    recent_set = recent_set.loc[recent_set[DATE] == last_month, :]
+        if isinstance(filtering_dataframe, pd.DataFrame) and not filtering_dataframe.empty:
+            filtering_dataframe = filtering_dataframe[[DATE, CODE]]
+            old_set = pd.merge(old_set, filtering_dataframe, on=[DATE, CODE])
 
-    if isinstance(filtering_dataframe, pd.DataFrame) and not filtering_dataframe.empty:
-        filtering_dataframe = filtering_dataframe[[DATE, CODE]]
-        old_set = pd.merge(old_set, filtering_dataframe, on=[DATE, CODE])
-        recent_set = pd.merge(old_set, filtering_dataframe, on=[DATE, CODE])
+        old_set.to_dataframe().to_hdf('data/{}.h5'.format(data_name), key='df', format='table', mode='w')
 
-    old_set.to_dataframe().to_hdf('data/{}.h5'.format(data_name), key='df', format='table', mode='w')
-    recent_set.to_dataframe().to_hdf('data/{}_recent.h5'.format(data_name), key='df', format='table', mode='w')
+    else:
+        # recent data
+        recent_set = get_data_set(portfolio, rolling_columns, dummy_columns, return_y=False)
+        # 마지막 달만 사용
+        last_month = np.sort(recent_set[DATE].unique())[-1]
+        recent_set = recent_set.loc[recent_set[DATE] == last_month, :]
+
+        if isinstance(filtering_dataframe, pd.DataFrame) and not filtering_dataframe.empty:
+            filtering_dataframe = filtering_dataframe[[DATE, CODE]]
+            recent_set = pd.merge(recent_set, filtering_dataframe, on=[DATE, CODE])
+
+        recent_set.to_dataframe().to_hdf('data/{}_recent.h5'.format(data_name), key='df', format='table', mode='w')
 
 
-def save_all():
+def save_all(old_data: bool):
     rolling_columns = [E_P, B_P, S_P, C_P, OP_P, GP_P, ROA, ROE, QROA, QROE, GP_A, ROIC, GP_S, SALESQOQ, GPQOQ, ROAQOQ,
                        MOM6, MOM12, BETA_1D, VOL_5M, LIQ_RATIO, EQUITY_RATIO, DEBT_RATIO, FOREIGN_OWNERSHIP_RATIO]
     portfolio = Portfolio()
@@ -94,10 +100,10 @@ def save_all():
     portfolio[TRADING_CAPITAL] = portfolio[TRADING_VOLUME_RATIO] * portfolio[MKTCAP]
     portfolio = portfolio.loc[portfolio[TRADING_CAPITAL] > 1000000000, :]
 
-    save_data(portfolio, ALL, rolling_columns)
+    save_data(old_data, portfolio, ALL, rolling_columns)
 
 
-def save_filter():
+def save_filter(old_data: bool):
     rolling_columns = [E_P, B_P, S_P, C_P, OP_P, GP_P, ROA, ROE, QROA, QROE, GP_A, ROIC, GP_S, SALESQOQ, GPQOQ, ROAQOQ,
                        MOM6, MOM12, BETA_1D, VOL_5M, LIQ_RATIO, EQUITY_RATIO, DEBT_RATIO, FOREIGN_OWNERSHIP_RATIO]
     portfolio = Portfolio()
@@ -115,10 +121,10 @@ def save_filter():
     # 0 < PSR < 0.8
     portfolio = portfolio.loc[portfolio[PSR] < 0.8]
 
-    save_data(portfolio, FILTER, rolling_columns)
+    save_data(old_data, portfolio, FILTER, rolling_columns)
 
 
-def save_bollinger():
+def save_bollinger(old_data: bool):
     rolling_columns = [E_P, B_P, S_P, C_P, OP_P, GP_P, ROA, ROE, QROA, QROE, GP_A, ROIC, GP_S, SALESQOQ, GPQOQ, ROAQOQ,
                        MOM6, MOM12, BETA_1D, VOL_5M, LIQ_RATIO, EQUITY_RATIO, DEBT_RATIO, FOREIGN_OWNERSHIP_RATIO]
     portfolio = Portfolio()
@@ -135,10 +141,10 @@ def save_bollinger():
     portfolio[BOLLINGER] = portfolio['mean'] - 2 * portfolio['std']
     bollingers = portfolio.loc[portfolio[ENDP] < portfolio[BOLLINGER], [DATE, CODE]]
 
-    save_data(portfolio, BOLLINGER, rolling_columns, filtering_dataframe=bollingers)
+    save_data(old_data, portfolio, BOLLINGER, rolling_columns, filtering_dataframe=bollingers)
 
 
-def save_sector():
+def save_sector(old_data: bool):
     columns = [DATE, CODE, RET_1]
     rolling_columns = [E_P, B_P, S_P, C_P, OP_P, GP_P, ROA, ROE, QROA, QROE, GP_A, ROIC, GP_S, SALESQOQ, GPQOQ, ROAQOQ,
                        MOM6, MOM12, BETA_1D, VOL_5M, LIQ_RATIO, EQUITY_RATIO, DEBT_RATIO, FOREIGN_OWNERSHIP_RATIO]
@@ -165,10 +171,10 @@ def save_sector():
     df_one_hot_encoded_sector = pd.DataFrame(one_hot_encoded_sector, columns=krx_sectors).reset_index(drop=True)
     portfolio[krx_sectors] = df_one_hot_encoded_sector
 
-    save_data(portfolio, SECTOR, rolling_columns, krx_sectors)
+    save_data(old_data, portfolio, SECTOR, rolling_columns, krx_sectors)
 
 
-def save_macro():
+def save_macro(old_data: bool):
     rolling_columns = [
         E_P, B_P, S_P, C_P, OP_P, GP_P, ROA, ROE, QROA, QROE, GP_A, ROIC, GP_S, SALESQOQ, GPQOQ, ROAQOQ,
         MOM6, MOM12, BETA_1D, VOL_5M, LIQ_RATIO, EQUITY_RATIO, DEBT_RATIO, FOREIGN_OWNERSHIP_RATIO,
@@ -182,10 +188,10 @@ def save_macro():
     portfolio[TRADING_CAPITAL] = portfolio[TRADING_VOLUME_RATIO] * portfolio[MKTCAP]
     portfolio = portfolio.loc[portfolio[TRADING_CAPITAL] > 1000000000, :]
 
-    save_data(portfolio, MACRO, rolling_columns)
+    save_data(old_data, portfolio, MACRO, rolling_columns)
 
 
-def save_concepts():
+def save_concepts(old_data: bool):
     portfolio = Portfolio()
     # 최소 시가총액 100억
     portfolio = portfolio.loc[portfolio[MKTCAP] > 10000000000, :]
@@ -216,13 +222,12 @@ def save_concepts():
                     factor_names.extend(mn)
                     factor_name = ''.join(factor_names)
                     if factor_name:
-                        factor_groups[factor_name[:-2]] = factor_group
+                        factor_groups[factor_name[:-1]] = factor_group
 
     factor_group_len = len(factor_groups)
 
-    with Pool(os.cpu_count()) as p:
-        # noinspection PyTypeChecker
-        rs = [p.apply_async(save_data, (pf, key, value)) for pf, (key, value) in zip(
+    with Pool(os.cpu_count() // 2) as p:
+        rs = [p.apply_async(save_data, [old_data, pf, key, value]) for pf, (key, value) in zip(
             [portfolio for _ in range(factor_group_len)],
             factor_groups.items()
         )]
@@ -233,9 +238,10 @@ def save_concepts():
 
 
 if __name__ == '__main__':
-    save_concepts()
-    with Pool(os.cpu_count()) as p:
-        results = [p.apply_async(func) for func in [
+    old_data = True
+    save_concepts(old_data=old_data)
+    with Pool(os.cpu_count() // 2) as p:
+        results = [p.apply_async(func, [old_data]) for func in [
             save_all, save_macro, save_filter, save_bollinger, save_sector
         ]]
         for result in results:
