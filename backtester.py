b3d924f133c0e4ad37e7bb744112d4f7fbcc2500
chanstar9 <traintion9@gmail.com>
2019-07-06 21:53:32 +0900

    error is revised

--- backtester.py
+++ backtester.py
@@ -8,14 +8,14 @@ from settings import *
 
 if __name__ == '__main__':
     backtest(param={
-        DATA_SET: 'value_size_momentum_quality_volatility',
+        DATA_SET: ALL,
         BATCH_SIZE: 300,
         EPOCHS: 100,
-        ACTIVATION: TAHN,
-        BIAS_INITIALIZER: ZEROS,
-        KERNEL_INITIALIZER: LECUN_NORMAL,
+        ACTIVATION: LINEAR,
+        BIAS_INITIALIZER: HE_UNIFORM,
+        KERNEL_INITIALIZER: GLOROT_UNIFORM,
         BIAS_REGULARIZER: NONE,
-        HIDDEN_LAYER: DNN8_4,
-        DROPOUT: True,
+        HIDDEN_LAYER: DNN8_1,
+        DROPOUT: False,
         DROPOUT_RATE: 0.5
-    }, start_number=0, end_number=119, max_pool=12)
+    }, start_number=0, end_number=9, max_pool=10)
